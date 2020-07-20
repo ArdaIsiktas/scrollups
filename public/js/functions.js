7fd0df5207131c18656e6028e3ca95a0b61ca4a9
Mert Gul <ibrahimmertgul@gmail.com>
2020-07-20 17:28:45 +0300

    Video deneme

--- public/js/functions.js
+++ public/js/functions.js
@@ -998,7 +998,29 @@ jQuery(function () {
 });
 */
 
+var frameNumber = 0, // start video at frame 0
+    // lower numbers = faster playback
+    playbackConst = 500, 
+    // get page height from video duration
+   //  setHeight = document.getElementById("video"), 
+    setHeight = document.getElementById("aboutus"), 
+    // select video element         
+    vid = document.getElementById('videos'); 
+    // var vid = $('#v0')[0]; // jquery option
+
+// dynamically set the page height according to video length
+vid.addEventListener('loadedmetadata', function() {
+  setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
+});
 
 
+// Use requestAnimationFrame for smooth playback
+function scrollPlay(){  
+  var frameNumber  = window.pageYOffset/playbackConst;
+  vid.currentTime  = frameNumber;
+  window.requestAnimationFrame(scrollPlay);
+}
+
+window.requestAnimationFrame(scrollPlay);
 
 
